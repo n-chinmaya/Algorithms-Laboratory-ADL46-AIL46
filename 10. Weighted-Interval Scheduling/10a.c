55f413fe85cad75deb0a0205a65718932324bb0a
Chinmaya Narmdeshwar <chinmayanarmdeshwar@gmail.com>
2024-08-01 15:40:29 +0530

    part a solution

---
+++ 10. Weighted-Interval Scheduling/10a.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to calculate f(n)
+int f(int n) {
+    return 5 * n * n + 6 * n + 3;
+}
+
+int c1_g(int n, int c1) {
+    return c1 * n * n;
+}
+
+int c2_g(int n, int c2){
+    return c2 * n * n;
+}
+
+int main() {
+    int n, c1 = 1, c2 = 14;
+    printf("n\tc1*g(n)\tf(n)\tc2*g(n)\n");
+    for (n = 10; n <= 30; n++) {
+        printf("%d\t%d\t%d\t%d\n", n, c1_g(n,c1), f(n), c2_g(n,c2));
+    }
+    return 0;
+}
