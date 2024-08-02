bb6359c9216b2f983a46b41ddf73b9d38aeadec6
Vedantrw <vedantrw2004@gmail.com>
2024-08-02 13:24:39 +0530

    Update 1a.c

--- 1. Gale-Shapley/1a.c
+++ 1. Gale-Shapley/1a.c
@@ -1,18 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int f(int n) {
-    return 7 * n + 5;
+int f(int n)
+{
+    return 7*n+5;
 }
 
-int g(int n, int c) {
-    return c * n;
+int g(int n,int c)
+{
+    return c*n;
 }
 
-int main() {
-    int n, c = 12;
-    printf("n\tf(n)\tc*g(n)\n");
-    for (n = 10; n <= 30; n++) {
-        printf("%d\t%d\t%d\n", n, f(n), g(n, c));
+int main()
+{
+    int c=8;
+    int n=1;
+    int n0=-1;
+    for(n=1;n<=30;n++)
+    {
+        if(f(n)<=g(n,c))
+        {
+            n0=n;
+            break;
+        }
     }
-    return 0;
+    printf("The value of n0 is %d\n",n0);
+
+    printf("n\tf(n)\tc*g(n)");
+    for(n=10;n<=30;n++)
+    {
+        printf("%d\t%d\t%d\n",n,f(n),g(n,c));
+    }
+
 }
